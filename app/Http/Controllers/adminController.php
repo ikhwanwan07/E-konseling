ccf6ff5e0b9dba7211ec81e31096d29c56f54192
Ikhwanwan07 <kenthoz07@gmail.com>
2022-07-02 03:03:17 +0700

    tambahin logic

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -17,291 +17,308 @@ use App\IPK;
 use App\Data;
 use Auth;
 use Alert;
+use App\Konsentrasi;
 use DB;
 
 
 class adminController extends Controller
 {
-    public function index(){
-      return view('op.index');
-      
-    }
-    public function show(){
+  public function index()
+  {
+    return view('op.index');
+  }
+  public function show()
+  {
 
-     $posts = Artikel::paginate(5);
-      return view('op.tabelpost',compact('posts'));
-    }
-    public function edit($id){
-      $post = \App\Artikel::find($id);
-      return view('op.edit',['post'=> $post]);
-      
-    }
-    public function tipe(){
-      return view('dosen.tipe');
-    }
-    public function ipk(){
+    $posts = Artikel::paginate(5);
+    return view('op.tabelpost', compact('posts'));
+  }
+  public function edit($id)
+  {
+    $post = \App\Artikel::find($id);
+    return view('op.edit', ['post' => $post]);
+  }
+  public function tipe()
+  {
+    return view('dosen.tipe');
+  }
+  public function ipk()
+  {
 
-      $mahasiswa = mahasiswa::all();
-      $ipk = \App\IPK::all();
-      return view('op.IPK',compact('mahasiswa','ipk'));
-    }
-    public function showipk($id){
+    $mahasiswa = mahasiswa::all();
+    $ipk = \App\IPK::all();
+    return view('op.IPK', compact('mahasiswa', 'ipk'));
+  }
+  public function showipk($id)
+  {
 
-      $mahasiswa = mahasiswa::find($id);
-      $mhs = mahasiswa::all();
-      //dd($mahasiswa->ipk);
-      
-      return view('op.showIPK',compact('mahasiswa','mhs'));
-    }
-    public function ipkcreate(Request $request){
-     
-      
-      //if (IPK::whereIn(['user_id','semester','tahun_ajaran'],$request->user_id,$request->semester,$request->tahun_ajaran)->exists()) {
-        //return redirect('/ipk')->with('info','data sudah ada');
-     //}
-     $ipk= \App\IPK::create($request->all());
-      return redirect('/ipk')->with('success','data berhasil ditambah');
-     
-      //return $ipk;
-      
-    }
+    $mahasiswa = mahasiswa::find($id);
+    $mhs = mahasiswa::all();
+    //dd($mahasiswa->ipk);
 
-    public function deleteipk($id){
+    return view('op.showIPK', compact('mahasiswa', 'mhs'));
+  }
+  public function ipkcreate(Request $request)
+  {
 
-      $ipk = IPK::find($id);
-      $ipk->delete();
-      return redirect('/ipk')->with('success','data berhasil didelete');
-    }
-    
-    public function jadwal(){
-      return view('op.jadwal');
-    }
-    public function nilai_mhs(){
-      $mhs = \App\mahasiswa::all();
-      $matkul = \App\Matkul::all();
-      return view('op.nilai_mhs',compact('mhs','matkul'));
-    }
-    public function showNilai($id){
-      $mhs1 = \App\mahasiswa::find($id);
-      $mhs = \App\mahasiswa::all();
-      $matkul = \App\Matkul::all();
-      return view('op.showNilai',compact('mhs','mhs1','matkul'));
-    }
-    public function tabel(){
 
-      $mahasiswa = Auth::user()->dosen->mahasiswa;
-      return view('dosen.tabel',compact('mahasiswa'));
-    }
-    public function profilemhs($id)
-    {
-      $lihat = mahasiswa::find($id);
-      $mhs = $lihat->matkul;
-      $ipk1 = $lihat->ipk;
-      
+    //if (IPK::whereIn(['user_id','semester','tahun_ajaran'],$request->user_id,$request->semester,$request->tahun_ajaran)->exists()) {
+    //return redirect('/ipk')->with('info','data sudah ada');
+    //}
+    $ipk = \App\IPK::create($request->all());
+    return redirect('/ipk')->with('success', 'data berhasil ditambah');
 
-      $data = [];
-      $data2 = [];
-      foreach($ipk1 as $m)
-      {
-        if($ipk1->first()){
+    //return $ipk;
+
+  }
+
+  public function deleteipk($id)
+  {
+
+    $ipk = IPK::find($id);
+    $ipk->delete();
+    return redirect('/ipk')->with('success', 'data berhasil didelete');
+  }
+
+  public function jadwal()
+  {
+    return view('op.jadwal');
+  }
+  public function nilai_mhs()
+  {
+    $mhs = \App\mahasiswa::all();
+    $matkul = \App\Matkul::all();
+    return view('op.nilai_mhs', compact('mhs', 'matkul'));
+  }
+  public function showNilai($id)
+  {
+    $mhs1 = \App\mahasiswa::find($id);
+    $mhs = \App\mahasiswa::all();
+    $matkul = \App\Matkul::all();
+    return view('op.showNilai', compact('mhs', 'mhs1', 'matkul'));
+  }
+  public function tabel()
+  {
+
+    $mahasiswa = Auth::user()->dosen->mahasiswa;
+    return view('dosen.tabel', compact('mahasiswa'));
+  }
+  public function profilemhs($id)
+  {
+    $lihat = mahasiswa::find($id);
+    $mhs = $lihat->matkul;
+    $ipk1 = $lihat->ipk;
+
+
+    $data = [];
+    $data2 = [];
+    foreach ($ipk1 as $m) {
+      if ($ipk1->first()) {
         $data[] = $m->semester;
         $data2[] = $m->ipk;
       }
-      }
-      //dd($data2);
-
-      return view('dosen.profile',compact('lihat','data','data2'));
     }
-    public function setting(){
-      return view('setting');
+    //dd($data2);
+
+    return view('dosen.profile', compact('lihat', 'data', 'data2'));
+  }
+  public function setting()
+  {
+    return view('setting');
+  }
+  public function tambah()
+  {
+
+    return view('op.post');
+  }
+  public function konsultasi()
+  {
+
+    $chat = Auth::user()->mahasiswa->pesan;
+
+    return view('mahasiswa.konsultasi', compact('chat'));
+  }
+  public function showchat($id)
+  {
+    $chat = \App\Chat::find($id);
+    $balas = \App\Balas::where('chat_id', $id)->get();
+    //dd($chat->id);
+
+    return view('mahasiswa.chat', compact('chat', 'balas'));
+  }
+  public function balas(Request $request)
+  {
+
+    $balas = new \App\Balas;
+    $balas->chat_id = $request->chat_id;
+    $balas->role = 'mahasiswa';
+    $balas->balas = $request->balas;
+    $balas->save();
+    //dd($balas);
+    return redirect('/konsultasi/' . $balas->chat_id);
+  }
+  public function konsentrasi()
+  {
+
+    $hasil = Auth::user()->mahasiswa->konsentrasi;
+
+    $data = [];
+
+    //dd($hasil);
+    foreach ($hasil as $key => $h) {
+
+      $data[$key]['name'] = $h->jalur;
+      $data[$key]['y'] = $h->nilai;
+
+      $key++;
     }
-    public function tambah(){
+    // dd($data);
 
-            return view('op.post');
-    }
-    public function konsultasi(){
+    return view('mahasiswa.konsentrasi', compact('data'));
+  }
 
-      $chat = Auth::user()->mahasiswa->pesan;
 
-      return view('mahasiswa.konsultasi',compact('chat'));
-    }
-    public function showchat($id)
-    {
-      $chat = \App\Chat::find($id);
-      $balas = \App\Balas::where('chat_id',$id)->get();
-      //dd($chat->id);
-      
-      return view('mahasiswa.chat',compact('chat','balas'));
-    }
-    public function balas(Request $request)
-    {
-     
-      $balas = new \App\Balas;
-      $balas->chat_id = $request->chat_id;
-      $balas->role = 'mahasiswa';
-      $balas->balas = $request->balas;  
-      $balas->save();
-      //dd($balas);
-      return redirect('/konsultasi/'.$balas->chat_id);
+  public function createkonsentrasi(Request $request)
+  {
+    $data = new \App\Konsentrasi;
+    $data1 = new \App\Konsentrasi;
+    $data2 = new \App\Konsentrasi;
+    $data3 = new \App\Konsentrasi;
+    $data4 = new \App\Konsentrasi;
+    $mahasiswa_id =  Auth::user()->mahasiswa->id;
 
-    }
-    public function konsentrasi()
-    {
+    $data->mahasiswa_id = Auth::user()->mahasiswa->id;
+    $data1->mahasiswa_id = Auth::user()->mahasiswa->id;
+    $data2->mahasiswa_id = Auth::user()->mahasiswa->id;
+    $data3->mahasiswa_id = Auth::user()->mahasiswa->id;
+    $data4->mahasiswa_id = Auth::user()->mahasiswa->id;
+    $data->jalur = 'magang';
+    $dataMagang = $request->magang1 + $request->magang2 + $request->magang3 + $request->magang4 + $request->magang5;
+    $data1->jalur = 'penelitian';
+    $dataPeneliti = $request->penelitian1 + $request->penelitian2 + $request->penelitian3 + $request->penelitian4 + $request->penelitian5;
+    $data2->jalur = 'perintisan bisnis';
+    $dataPb = $request->pb1 + $request->pb2 + $request->pb3 + $request->pb4 + $request->pb5;
+    $data3->jalur = 'kuliah di luar negeri';
+    $dataKuliah = $request->kuliah1 + $request->kuliah2 + $request->kuliah3 + $request->kuliah4 + $request->kuliah5;
+    $data4->jalur = 'pengabdian';
+    $dataPengabdian = $request->pengabdian1 + $request->pengabdian2 + $request->pengabdian3 + $request->pengabdian4 + $request->pengabdian5;
 
-      $hasil = Auth::user()->mahasiswa->konsentrasi;
+    $total = $dataMagang + $dataKuliah + $dataPb + $dataPeneliti + $dataPengabdian;
+    $data->nilai = $dataMagang * 100 / $total;
+    $data1->nilai = $dataPeneliti * 100 / $total;
+    $data2->nilai = $dataPb * 100 / $total;
+    $data3->nilai = $dataKuliah * 100 / $total;
+    $data4->nilai = $dataPengabdian * 100 / $total;
 
-      $data = [];
-     
-//dd($hasil);
-      foreach($hasil as $key => $h)
-      { 
 
-        $data[$key]['name'] = $h->jalur;
-        $data[$key]['y'] = $h->nilai;
-
-        $key++;
-        
-      }
-      // dd($data);
-    
-      return view('mahasiswa.konsentrasi',compact('data'));
-    }
-    
-
-    public function createkonsentrasi(Request $request)
-    {
-      $data = new \App\Konsentrasi;
-      $data1 = new \App\Konsentrasi;
-      $data2 = new \App\Konsentrasi;
-      $data3 = new \App\Konsentrasi;
-      $data4 = new \App\Konsentrasi;
-      $data->mahasiswa_id = Auth::user()->mahasiswa->id;
-      $data1->mahasiswa_id = Auth::user()->mahasiswa->id;
-      $data2->mahasiswa_id = Auth::user()->mahasiswa->id;
-      $data3->mahasiswa_id = Auth::user()->mahasiswa->id;
-      $data4->mahasiswa_id = Auth::user()->mahasiswa->id;
-      $data->jalur = 'magang';
-      $dataMagang = $request->magang1 + $request->magang2 + $request->magang3 +$request->magang4 +$request->magang5;
-      $data1->jalur = 'penelitian';
-      $dataPeneliti = $request->penelitian1 + $request->penelitian2 + $request->penelitian3 +$request->penelitian4 +$request->penelitian5;
-      $data2->jalur = 'perintisan bisnis';
-      $dataPb = $request->pb1 + $request->pb2 + $request->pb3 +$request->pb4 +$request->pb5;
-      $data3->jalur = 'kuliah di luar negeri';
-      $dataKuliah = $request->kuliah1 + $request->kuliah2 + $request->kuliah3 +$request->kuliah4 +$request->kuliah5;
-      $data4->jalur = 'pengabdian';
-      $dataPengabdian = $request->pengabdian1 + $request->pengabdian2 + $request->pengabdian3 +$request->pengabdian4 +$request->pengabdian5;
-
-      $total = $dataMagang + $dataKuliah + $dataPb + $dataPeneliti + $dataPengabdian;
-      $data->nilai = $dataMagang * 100 / $total;
-      $data1->nilai = $dataPeneliti * 100 / $total;
-      $data2->nilai = $dataPb * 100 / $total;
-      $data3->nilai = $dataKuliah * 100 / $total;
-      $data4->nilai = $dataPengabdian * 100 / $total;
-
-      
+    if (Konsentrasi::where('mahasiswa_id', $mahasiswa_id)->exists()) {
+      // post with the same slug already exists
+      return redirect()->back()->with('info', 'data konsentrasi sudah ada');
+    } else {
       $data->save();
       $data1->save();
       $data2->save();
       $data3->save();
       $data4->save();
-     // dd($data,$data1,$data2,$data3,$data4);
-      return redirect('/konsentrasi');
-      
+      // dd($data,$data1,$data2,$data3,$data4);
+      return redirect('/konsentrasi')->with('success', 'data berhasil di tambah');
     }
-    
-  
-    public function postBerita(Request $request){
-
-     Artikel::create([
-       'title' => request('title'),
-       'kategori' => request('kategori'),
-       'content' => request('content'),
-       
-     ]);
-
-        return redirect('tabelpost');
-    }
-    public function update($id, Request $request){
-      $post = \App\Artikel::find($id);
-      $post->update($request->all());
-      return redirect ('tabelpost');
-
-    }
-    public function delete($id)
-    {
-      $post = \App\Artikel::find($id);
-      $post->delete();
-      return redirect('tabelpost')->with('sukses','data berhasil didelete');
-    }
-    public function sistem(){
-
-      $data = \App\Data::all();
-      return view('op.sistem',compact('data'));
-      
-    }
-
-    public function proses($cluster, $iteration = null, $centroids = null)
-    {
-      //get data
-      $data = Data::all()->toArray();
-
-      //init kmeans object
-      $kmeans = new KMeans($data, $cluster);
-      //set label berdasarkan data (berdasarkan nama kolom pada data)
-      $kmeans->setLabel('nim');
-      // set attribute untuk data matriks clustering (berdasarkan nama kolom pada data)
-      $kmeans->setAttributes(['ipk_sem_1','ipk_sem_2','ipk_sem_3','ipk_sem_4','ipk_sem_5']);
-      
-      $custom = [];
-      if($iteration != null){
-        $custom[] = $iteration;
-      }
-
-      if($centroids != null){
-        $array_centroid = explode(',',$centroids);
-        if(sizeof($array_centroid) != $cluster){
-          return ['status'=>'error', 'message'=>'Jumlah centroid harus sama dengan cluster!'];
-        }
-        if(in_array('',$array_centroid) || in_array(null,$array_centroid)){
-          return ['status'=>'error', 'message'=>'Centroid harus terisi semua!'];
-        }
-        $custom[] = $array_centroid;
-      }
-
-      $kmeans->setCustom($custom);
-
-       //return hasil clustering
-      return $kmeans->clustering();
-    }
-
-public function export_excel()
-	{
-		return Excel::download(new mahasiswaExport, 'siswa.xlsx');
   }
-  
-  public function import_excel(Request $request) 
-	{
+
+
+  public function postBerita(Request $request)
+  {
+
+    Artikel::create([
+      'title' => request('title'),
+      'kategori' => request('kategori'),
+      'content' => request('content'),
+
+    ]);
+
+    return redirect('tabelpost');
+  }
+  public function update($id, Request $request)
+  {
+    $post = \App\Artikel::find($id);
+    $post->update($request->all());
+    return redirect('tabelpost');
+  }
+  public function delete($id)
+  {
+    $post = \App\Artikel::find($id);
+    $post->delete();
+    return redirect('tabelpost')->with('sukses', 'data berhasil didelete');
+  }
+  public function sistem()
+  {
+
+    $data = \App\Data::all();
+    return view('op.sistem', compact('data'));
+  }
+
+  public function proses($cluster, $iteration = null, $centroids = null)
+  {
+    //get data
+    $data = Data::all()->toArray();
+
+    //init kmeans object
+    $kmeans = new KMeans($data, $cluster);
+    //set label berdasarkan data (berdasarkan nama kolom pada data)
+    $kmeans->setLabel('nim');
+    // set attribute untuk data matriks clustering (berdasarkan nama kolom pada data)
+    $kmeans->setAttributes(['ipk_sem_1', 'ipk_sem_2', 'ipk_sem_3', 'ipk_sem_4', 'ipk_sem_5']);
+
+    $custom = [];
+    if ($iteration != null) {
+      $custom[] = $iteration;
+    }
+
+    if ($centroids != null) {
+      $array_centroid = explode(',', $centroids);
+      if (sizeof($array_centroid) != $cluster) {
+        return ['status' => 'error', 'message' => 'Jumlah centroid harus sama dengan cluster!'];
+      }
+      if (in_array('', $array_centroid) || in_array(null, $array_centroid)) {
+        return ['status' => 'error', 'message' => 'Centroid harus terisi semua!'];
+      }
+      $custom[] = $array_centroid;
+    }
+
+    $kmeans->setCustom($custom);
+
+    //return hasil clustering
+    return $kmeans->clustering();
+  }
+
+  public function export_excel()
+  {
+    return Excel::download(new mahasiswaExport, 'siswa.xlsx');
+  }
+
+  public function import_excel(Request $request)
+  {
     //Remove all data first
     Data::truncate();
-    
+
     // validasi
-		$this->validate($request, [
-			'file' => 'required|mimes:csv,xls,xlsx'
-		]);
- 
-		// menangkap file excel
-		$file = $request->file('file');
- 
-		// membuat nama file unik
-		$nama_file = rand().$file->getClientOriginalName();
- 
-		// upload ke folder file_siswa di dalam folder public
-		$file->move('file_cluster',$nama_file);
- 
-		// import data
-		Excel::import(new mahasiswaImport, public_path('/file_cluster/'.$nama_file));
- 
-		// alihkan halaman kembali
-		return redirect('/sistem')->with('success','Data Berhasil Diimport!');
-	}
+    $this->validate($request, [
+      'file' => 'required|mimes:csv,xls,xlsx'
+    ]);
+
+    // menangkap file excel
+    $file = $request->file('file');
+
+    // membuat nama file unik
+    $nama_file = rand() . $file->getClientOriginalName();
+
+    // upload ke folder file_siswa di dalam folder public
+    $file->move('file_cluster', $nama_file);
+
+    // import data
+    Excel::import(new mahasiswaImport, public_path('/file_cluster/' . $nama_file));
+
+    // alihkan halaman kembali
+    return redirect('/sistem')->with('success', 'Data Berhasil Diimport!');
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,81 +12,76 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('masuk');
 });
 
 
 
-Route::get('/login',"AuthController@login")->name('login');
-Route::post('/postLogin',"AuthController@postLogin");
-Route::get('/logout',"AuthController@logout");
+Route::get('/login', "AuthController@login")->name('login');
+Route::post('/postLogin', "AuthController@postLogin");
+Route::get('/logout', "AuthController@logout");
 
-Route::group(['middleware'=>['auth','CheckRole:admin']],function(){
-Route::get('/dashboard',"adminController@index");
-Route::get('/post',"adminController@post");
-Route::get('/mahasiswa',"mahasiswaController@index");
-Route::post('/mahasiswa/create',"mahasiswaController@create");
-Route::get('/mahasiswa/{id}/edit',"mahasiswaController@edit");
-Route::post('/mahasiswa/{id}/update',"mahasiswaController@update");
-Route::get('/mahasiswa/{id}/hapus',"mahasiswaController@delete");
-Route::get('/dosen',"dosenController@index");
-Route::post('/dosen/create',"dosenController@create");
-Route::get('/dosen/{id}/edit',"dosenController@edit");
-Route::post('/dosen/{id}/update',"dosenController@update");
-Route::get('/dosen/{id}/hapus',"dosenController@delete");
-Route::get('/tabelpost ',"adminController@show")->name('tabelpost');
-Route::get('/post',"adminController@tambah");
-Route::get('/postprestasi',"adminController@tambahprestasi");
-Route::post('/postBerita',"adminController@postBerita")->name('post.berita');
-Route::get('/Berita/{id}/edit',"adminController@edit");
-Route::post('/Berita/{id}/update',"adminController@update");
-Route::get('/Berita/{id}/hapus',"adminController@delete");
-Route::get('/ipk',"adminController@ipk");
-Route::get('/ipk/{id}',"adminController@showipk");
-Route::post('/ipkcreate',"adminController@ipkcreate");
-Route::get('/ipk/{id}/hapus',"adminController@deleteipk");
-Route::get('/jadwal',"adminController@jadwal");
-Route::get('/nilai_mhs',"adminController@nilai_mhs");
-Route::get('/nilai/{id}',"adminController@showNilai");
-Route::get('/sistem',"adminController@sistem");
-Route::get('/proses/{cluster}',"adminController@proses");
-Route::get('/proses/{cluster}/iteration/{iteration}',"adminController@proses");
-Route::get('/proses/{cluster}/centroids/{centroids}',"adminController@proses");
-Route::get('/proses/{cluster}/iteration/{iteration}/centroids/{centroids}',"adminController@proses");
-Route::get('/mahasiswa/export_excel', 'adminController@export_excel');
-Route::post('/mahasiswa/import_excel', 'adminController@import_excel');
-
+Route::group(['middleware' => ['auth', 'CheckRole:admin']], function () {
+    Route::get('/dashboard', "adminController@index");
+    Route::get('/post', "adminController@post");
+    Route::get('/mahasiswa', "mahasiswaController@index");
+    Route::post('/mahasiswa/create', "mahasiswaController@create");
+    Route::get('/mahasiswa/{id}/edit', "mahasiswaController@edit");
+    Route::post('/mahasiswa/{id}/update', "mahasiswaController@update");
+    Route::get('/mahasiswa/{id}/hapus', "mahasiswaController@delete");
+    Route::get('/dosen', "dosenController@index");
+    Route::post('/dosen/create', "dosenController@create");
+    Route::get('/dosen/{id}/edit', "dosenController@edit");
+    Route::post('/dosen/{id}/update', "dosenController@update");
+    Route::get('/dosen/{id}/hapus', "dosenController@delete");
+    Route::get('/tabelpost ', "adminController@show")->name('tabelpost');
+    Route::get('/post', "adminController@tambah");
+    Route::get('/postprestasi', "adminController@tambahprestasi");
+    Route::post('/postBerita', "adminController@postBerita")->name('post.berita');
+    Route::get('/Berita/{id}/edit', "adminController@edit");
+    Route::post('/Berita/{id}/update', "adminController@update");
+    Route::get('/Berita/{id}/hapus', "adminController@delete");
+    Route::get('/ipk', "adminController@ipk");
+    Route::get('/ipk/{id}', "adminController@showipk");
+    Route::post('/ipkcreate', "adminController@ipkcreate");
+    Route::get('/ipk/{id}/hapus', "adminController@deleteipk");
+    Route::get('/jadwal', "adminController@jadwal");
+    Route::get('/nilai_mhs', "adminController@nilai_mhs");
+    Route::get('/nilai/{id}', "adminController@showNilai");
+    Route::get('/sistem', "adminController@sistem");
+    Route::get('/proses/{cluster}', "adminController@proses");
+    Route::get('/proses/{cluster}/iteration/{iteration}', "adminController@proses");
+    Route::get('/proses/{cluster}/centroids/{centroids}', "adminController@proses");
+    Route::get('/proses/{cluster}/iteration/{iteration}/centroids/{centroids}', "adminController@proses");
+    Route::get('/mahasiswa/export_excel', 'adminController@export_excel');
+    Route::post('/mahasiswa/import_excel', 'adminController@import_excel');
 });
 
-Route::group(['middleware'=>['auth','CheckRole:dosen,admin,mahasiswa']],function(){
-    Route::get('/dashboard',"adminController@index");
-    Route::get('/tipe',"adminController@tipe");
-    Route::get('/konseling',"dosenController@konseling");
-    Route::get('/konseling/{id}',"dosenController@showchat")->name('showChat');
-    Route::get('/konsultasi/{id}',"adminController@showchat")->name('Chatmhs');
-    Route::get('/konsultasi',"adminController@konsultasi");
-    Route::get('/chat',"dosenController@chat");
-    Route::post('/balas',"dosenController@balas");
-    Route::post('/balasmhs',"adminController@balas");
-    Route::get('/jadwalkonsultasi',"dosenController@jadwal");
-    Route::get('/konsentrasi',"adminController@konsentrasi");
-    Route::get('/tabel',"adminController@tabel");
-    Route::get('/profile/{id}',"adminController@profilemhs")->name('showProfile');
-    Route::get('/berita',"mahasiswaController@berita");
-    Route::get('/profile',"mahasiswaController@profile");
-    Route::post('/createkonsentrasi','adminController@createkonsentrasi');
-    Route::post('/chat',"mahasiswaController@chat");
-    Route::get('/nilai',"mahasiswaController@nilai");
-    Route::get('/Berita/{id}',"mahasiswaController@lihat")->name('lihat');
-    Route::get('/setting',"adminController@setting");
-    Route::post('/tambahjadwal',"dosenController@tambahjadwal");
-    Route::get('/jadwal/{id}/edit',"dosenController@editjadwal");
-    Route::post('/jadwal/{id}/update',"dosenController@updatejadwal");
-    Route::get('/jadwal/{id}/hapus',"dosenController@deletejadwal");
-
-
-    
-
+Route::group(['middleware' => ['auth', 'CheckRole:dosen,admin,mahasiswa']], function () {
+    Route::get('/dashboard', "adminController@index");
+    Route::get('/tipe', "adminController@tipe");
+    Route::get('/konseling', "dosenController@konseling");
+    Route::get('/konseling/{id}', "dosenController@showchat")->name('showChat');
+    Route::get('/konsultasi/{id}', "adminController@showchat")->name('Chatmhs');
+    Route::get('/konsultasi', "adminController@konsultasi");
+    Route::get('/chat', "dosenController@chat");
+    Route::post('/balas', "dosenController@balas");
+    Route::post('/balasmhs', "adminController@balas");
+    Route::get('/jadwalkonsultasi', "dosenController@jadwal");
+    Route::get('/konsentrasi', "adminController@konsentrasi");
+    Route::get('/tabel', "adminController@tabel");
+    Route::get('/profile/{id}', "adminController@profilemhs")->name('showProfile');
+    Route::get('/berita', "mahasiswaController@berita");
+    Route::get('/profile', "mahasiswaController@profile");
+    Route::post('/createkonsentrasi', 'adminController@createkonsentrasi');
+    Route::post('/chat', "mahasiswaController@chat");
+    Route::get('/nilai', "mahasiswaController@nilai");
+    Route::get('/Berita/{id}', "mahasiswaController@lihat")->name('lihat');
+    Route::get('/setting', "adminController@setting");
+    Route::post('/tambahjadwal', "dosenController@tambahjadwal");
+    Route::get('/jadwal/{id}/edit', "dosenController@editjadwal");
+    Route::post('/jadwal/{id}/update', "dosenController@updatejadwal");
+    Route::get('/jadwal/{id}/hapus', "dosenController@deletejadwal");
 });
 
 
@@ -95,4 +90,3 @@ Route::group(['middleware'=>['auth','CheckRole:dosen,admin,mahasiswa']],function
 Route::get('/home', 'HomeController@index')->name('home');
 
 //Auth::routes();
-
